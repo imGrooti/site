61119644917c179ead4321cacc4641510375b812
imGrooti <harish12groot@gmail.com>
2023-09-08 13:57:22 +0530

    m

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import "../components/Footer.css"
+import "../components/Footer.css";
+import groot from "../Assets/groot.png";
 let time= new Date();
 
 const Footer = () => {
   return (
     <div>
+        <p>Made By imGroot  <img src={groot} alt="groot" /></p>
+       
 
         <p>copyrights © {time.getFullYear()}</p>
+        
     </div>
   )
 }
